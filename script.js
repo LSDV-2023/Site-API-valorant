c393aec72a6b53ee6997edbe7946e1ba8573d3e7
Aldin285 <148047232+Aldin285@users.noreply.github.com>
2024-01-24 10:46:33 +0100

    debut test pour API

---
+++ script.js
@@ -0,0 +1,13 @@
+fetch("https://valorant-api.com/v1/weapons")
+  .then((response) => {
+    if (response.ok) {
+      return response.json();
+    } else {
+      throw new Error("NETWORK RESPONSE ERROR");
+    }
+  })
+  .then(data => {
+    console.log(data);
+    
+  })
+  .catch((error) => console.error("FETCH ERROR:", error));
