fbce7ce4b66f5c937ea77b2bcd85921576b69faa
Aldin285 <148047232+Aldin285@users.noreply.github.com>
2024-01-24 11:34:50 +0100

    Extraction des noms de l'API
    j'ai pu prendre le nom d'une arme avec L'API mais l'image ne vient pas

--- script.js
+++ script.js
@@ -8,6 +8,21 @@ fetch("https://valorant-api.com/v1/weapons")
   })
   .then(data => {
     console.log(data);
-    
+    displayWeapon(data)
   })
   .catch((error) => console.error("FETCH ERROR:", error));
+
+  function displayWeapon(data) {
+    const weapon = data.data[0];
+    const weaponDiv = document.getElementById("weapon"); 
+    // weapon name
+    const weaponName = weapon.displayName;
+    const heading = document.createElement("h1");
+    heading.innerHTML = weaponName;
+    weaponDiv.appendChild(heading);
+    // weapon image
+    const weaponImg = document.createElement("img");
+    weaponImg.src = weapon.newImage;
+    weaponDiv.appendChild(weaponImg);
+    document.body.style.backgroundImage = "url('" + weapon.newImage + "')";
+  }
