5b94ab69095d17a82ce330e296cbcc088514a0f1
Aldin285 <148047232+Aldin285@users.noreply.github.com>
2024-01-27 20:49:44 +0100

    affichage de toutes les armes avec nom+ Erreure non réglée
    j'ai pu afficher toute les armes mes il reste un probleme avec la derniere arme "Melee". on ajoutant l'élement Melee je reçois une erreure car il ne contient pas d'image alors je ne l'affiche pas pour le moment

--- script.js
+++ script.js
@@ -13,17 +13,20 @@ fetch("https://valorant-api.com/v1/weapons")
   .catch((error) => console.error("FETCH ERROR:", error));
 
   function displayWeapon(data) {
-    const weapon = data.data[1];
-    const weaponDiv = document.getElementById("weapon"); 
-    // weapon name
-    const weaponName = weapon.displayName;
-    const heading = document.createElement("h1");
-    heading.innerHTML = weaponName;
-    weaponDiv.appendChild(heading);
-    // weapon image
-    const weaponImg = document.createElement("img");
-    weaponImg.src = weapon.shopData.newImage;
-    weaponDiv.appendChild(weaponImg);
-    // arrierre plan de la page
-    // document.body.style.backgroundImage = "url('" + weapon.shopData.newImage + "')";
+    for (let i = 0;i < data.data.length-1 ; i++ ){
+      const weapon = data.data[i];
+      const weaponDiv = document.getElementById("weapon"); 
+      // weapon name
+      const weaponName = weapon.displayName;
+      const heading = document.createElement("h1");
+      heading.innerHTML = weaponName;
+      weaponDiv.appendChild(heading);
+      // weapon image
+      const weaponImg = document.createElement("img");
+      weaponImg.src = weapon.shopData.newImage;
+      weaponDiv.appendChild(weaponImg);
+      
+      // arrierre plan de la page
+      // document.body.style.backgroundImage = "url('" + weapon.shopData.newImage + "')";
+    }
   }
