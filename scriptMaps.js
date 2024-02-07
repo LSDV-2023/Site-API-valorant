cf79d2d2a118d43a741d0c49aeda2eb5393663b5
Aldin285 <148047232+Aldin285@users.noreply.github.com>
2024-02-07 13:11:14 +0100

    réglage de l'affichage dans mapsTest

--- scriptMaps.js
+++ scriptMaps.js
@@ -28,14 +28,69 @@ fetch("https://valorant-api.com/v1/maps")
     
     //       return img
     //     }
+<<<<<<< Updated upstream
     //       function maps_N (n){
     //       const h1=document.createElement("h1");
     //       h1.appendChild(document.createTextNode(n));
+=======
+    // }
+
+    // function weapon_images(data){
+    //     for (let i = 0;i < data.data.length-1 ; i++ ){
+
+    //         const weapon = data.data[i];
+
+    //         function weapon_image (p){
+    //            const img = document.createElement("img");
+    //            img.src = p;
+    //            img.classList.add("image-grid")
+    //            return img
+    //          }
+
+    //         function weapon_div (p){
+    //             const div = document.createElement("div");
+                
+    //             div.appendChild(weapon_image(p));
+
+    //             return div
+    //         }
+
+    //         function weapon_div2(p){
+    //           const div2 = document.createElement("div");
+    //           div2.classList.add("item1");
+    //           div2.appendChild(weapon_div(p));
+
+    //           return div2
+    //         }
+
+    //         const imagerie = document.querySelector("#weapon_image");
+    //         imagerie.appendChild(weapon_div2(weapon.shopData.newImage))
+            
+    //     }
+    // }
+      function maps_name_image(data){
+        for (let i = 0;i < data.data.length-1 ; i++ ){
+    
+          const maps = data.data[i];
+    
+    
+        function maps_I (i){
+          const img = document.createElement("img");
+          img.src = i;
+          
+    
+          return img
+        }
+          function maps_N (n){
+          const h1=document.createElement("h1");
+          h1.appendChild(document.createTextNode(n));
+>>>>>>> Stashed changes
           
     
     //       return h1
     //     }
     
+<<<<<<< Updated upstream
     //     function maps_div_NI (n,i){
     //       const divI=document.createElement("span");
     //       divI.appendChild(maps_I(i))
@@ -43,6 +98,17 @@ fetch("https://valorant-api.com/v1/maps")
     //       const divI2=document.createElement("span");
     //       divI2.classList.add("item1");
     //       divI2.appendChild(divI)
+=======
+        function maps_div_NI (n,i){
+          const divI=document.createElement("span");
+          divI.appendChild(maps_I(i));
+          divI.classList.add("image-grid");
+    
+    
+          const divI2=document.createElement("span");
+          divI2.classList.add("item1");
+          divI2.appendChild(divI);
+>>>>>>> Stashed changes
     
     //       const divN = document.createElement("div");
     //       divN.appendChild(maps_N(n));
@@ -52,6 +118,7 @@ fetch("https://valorant-api.com/v1/maps")
           
     //     }
     
+<<<<<<< Updated upstream
     //     const affichage = document.querySelector("#maps_name_image");
     //     const t = maps_div_NI(maps.displayName,maps.shopData.newImage);
     //     affichage.appendChild(t[0]);
@@ -90,6 +157,12 @@ fetch("https://valorant-api.com/v1/maps")
           const affichage = document.querySelector("#maps_name_image");
           const mapDiv = map_div(map.displayName, map.splash);
           affichage.appendChild(mapDiv);
+=======
+        const affichage = document.querySelector("#maps_name_image");
+        const t = maps_div_NI(maps.displayName,maps.splash);
+        affichage.appendChild(t[0]);
+        affichage.appendChild(t[1]);
+>>>>>>> Stashed changes
       }
   }
   
